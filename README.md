f5cc1395ec80dd1df76f1edd93990dc02ddc8295
Darryl Ross <darryl@afoyi.com>
2014-03-18 09:03:02 +1030

    Add NTP module contents.

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
-### NTP
+# NTP module for Puppet
 
-NTP puppet module.
+This is puppet module for configuring NTP on servers. It defines a new type, called ntp, that provides the required functionality. This is designed to be instantiated into the base node with the FQDN as part of the instance name so that variables can be over-ridden on a per node basis as required. This is a work around for puppet not allowing classes inherited from parents to be overridden.
+
+Currently only tested on Debian/Ubuntu and RedHat/CentOS based servers.
+
+
+## Puppet Types
+
+1. `ntp`
+
+    Object that performs the configuration of the NTP service on the node. Supports the following options:
+
+    * `servers` (array)
+
+        The list of upstream NTP servers for the node (generates server lines)
+
+    * `restrict` (array)
+
+        The list of trusted clients for this node (generates restrict lines)
+
+    * `keysfile` (string)
+
+        The location of the key file for this node (generates the keysfile line)
+
+    * `driftfile` (string)
+
+        The location of the drift file for this node (generates the driftfile line)
+
+    * `preferred_servers` (array)
+
+        A list of the preferred servers for this node (Adds the 'prefer' keyword to servers in the `servers` list)
+
+## Examples
+
+### site.pp
+
+This snippet includes the NTP class into the default node, which is inherited by all nodes. This ensures that all servers are configured for NTP, using our set of internal servers. It also defines a variable which contains the NTP servers that our internal NTP servers use themselves:
+
+
+    node default {
+        $ntp_service_servers = [ 'ntp0.cs.mu.oz.au', 'ntp1.cs.mu.oz.au', 'clock.via.net', 'time.nist.gov', 'ntp.on.net', 'time.deakin.edu.au' ]
+        ntp { "ntp_${fqdn}" :
+            servers   => [ 'ntp1.afoyi.com', 'ntp2.afoyi.com', 'ntp3.afoyi.com' ],
+            restrict  => [ '172.17.1.2', '103.1.212.18', '10.254.254.5', '2403:4200:403:2::5' ],
+        }
+    }
+
+### node
+
+This snippet demonstrates overriding ones of the variables, such as the server list, for a single node, such as the NTP servers listed in the default node:
+
+    node "ntp1.afoyi.com" {
+        Ntp["ntp_$fqdn"] { 
+            servers => $ntp_service_servers
+        }
+    }
+
---
+++ manifests/init.pp
@@ -0,0 +1,74 @@
+# NTP Service Definition
+#  Allows overriding variables on a per-host basis.
+define ntp ($servers,
+            $restrict=[],
+            $keysfile='/etc/ntp/keys',
+            $driftfile='/var/lib/ntp/drift',
+            $preferred_servers=[]) {
+
+    package { 'ntp' :
+        ensure => $::is_virtual ? {
+            true     => absent,
+            default  => latest
+        }
+    }
+
+    case $::is_virtual {
+        false: {
+            service { 'ntpd' :
+                name      => $::osfamily ? {
+                    RedHat  => 'ntpd',
+                    default => 'ntp',
+                },
+                ensure    => running,
+                enable    => true,
+                require   => Package['ntp'],
+                subscribe => File['ntp.conf'],
+            }
+
+            file { 'ntp.conf' :
+                ensure  => present,
+                mode    => '0644',
+                owner   => 'root',
+                group   => 'root',
+                require => Package['ntp'],
+                path    => '/etc/ntp.conf',
+                content => template('puppet_header.tpl', 'ntp/ntp.conf.erb'),
+            }
+
+            file { 'ntp_keysdir':
+                ensure  => directory,
+                mode    => '0755',
+                owner   => 'root',
+                group   => 'root',
+                require => Package['ntp'],
+                path    => inline_template('<%= File.basename(@keysfile) %>'),
+            }
+            file { 'ntp_keysfile':
+                ensure  => present,
+                mode    => '0600',
+                owner   => 'root',
+                group   => 'root',
+                require => [ Package['ntp'], File['ntp_keysdir'] ],
+                path    => $keysfile,
+            }
+
+            file { 'ntp_driftdir':
+                ensure  => directory,
+                mode    => '0755',
+                owner   => 'ntp',
+                group   => 'ntp',
+                require => Package['ntp'],
+                path    => inline_template('<%= File.basename(@driftfile) %>'),
+            }
+            file { 'ntp_driftfile':
+                ensure  => present,
+                mode    => '0644',
+                owner   => 'ntp',
+                group   => 'ntp',
+                require => [ Package['ntp'], File['ntp_driftdir'] ],
+                path    => $driftfile,
+            }
+        }
+    }
+}
